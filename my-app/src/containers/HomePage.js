847fd15bfd65743d85c05653c9d084f6a2764fd0
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-02-26 16:50:37 +0700

    Add counter functionality

--- my-app/src/containers/HomePage.js
+++ my-app/src/containers/HomePage.js
@@ -1,7 +1,26 @@
+import React, { useState } from 'react';
+
 const HomePage = () => {
+
+  const [ count, setCount ] = useState(0);
+
+  const increment = () => {
+    setCount(prev => prev + 1);
+  }
+
+  const decrement = () => {
+    setCount(prev => prev - 1);
+  }
+
   return (
     <div>
       <h1>Home Page</h1>
+      <div>
+        <button onClick={decrement}>-</button>
+          <span>{count}</span>
+        <button onClick={increment}>+</button>
+
+      </div>
     </div>
   );
 }
